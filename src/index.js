36d46840c70da93f5005651b1f5657cc91427147
goongoguma <wogus7an@gmail.com>
2018-11-06 17:04:26 +0900

    새 글 추가하기

--- src/index.js
+++ src/index.js
@@ -71,6 +71,7 @@ async function drawPostList() {
 
   // 2. 요소 선택
   const listEl = frag.querySelector('.post-list') // tbody선택
+  const createEl = frag.querySelector('.create');
 
   // 3. 필요한 데이터 불러오기
   // data속성을 꺼내와 postList 변수에 할당
@@ -100,7 +101,9 @@ async function drawPostList() {
   }
 
   // 5. 이벤트 리스너 등록하기
-  
+  createEl.addEventListener('click', e => {
+    drawNewPostForm();
+  })
 
   // 6. 템플릿을 문서에 삽입
   rootEl.textContent = ''
@@ -188,13 +191,29 @@ async function drawPostDetail(postId) {
   rootEl.appendChild(frag);
 }
  
+// 새 글 추가
 async function drawNewPostForm() {
   // 1. 템플릿 복사
+  const frag = document.importNode(templates.postForm, true);
   // 2. 요소 선택
+  const formEl = frag.querySelector('.post-form');
+
   // 3. 필요한 데이터 불러오기
   // 4. 내용 채우기
   // 5. 이벤트 리스너 등록하기
+  formEl.addEventListener('submit', async e => {
+    e.preventDefault();
+    const title = e.target.elements.title.value;
+    const body = e.target.elements.body.value;
+    await api.post('/posts', {
+      title,
+      body
+    })
+    drawPostList();
+  })
   // 6. 템플릿을 문서에 삽입
+  rootEl.textContent = '';
+  rootEl.appendChild(frag);
 }
 
 async function drawEditPostForm(postId) {
